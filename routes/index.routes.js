972e5f458767a798e49e35ff7959b9e296dff600
Chaudo <a.chaudoreille@gmail.com>
2023-02-20 15:36:41 +0100

    add skeleton files for routes (#4)

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+    res.render("index");
 });
+
+// page routing
+router.use("/", require("./authentication.routes"));
+router.use("/user", require("./user.routes"));
+router.use("/lists", require("./lists.routes"));
+router.use("/saved", require("./saved.routes"));
+router.use("/recipes", require("./recipes.routes"));
+
+// api routing
+router.use("/api/lists", require("./api/lists.routes"));
+router.use("/api/recipes", require("./api/recipes.routes"));
+router.use("/api/tags", require("./api/tags.routes"));
+router.use("/api/ingredients", require("./api/ingredients.routes"));
 
 module.exports = router;

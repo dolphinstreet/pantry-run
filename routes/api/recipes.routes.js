b74087a1492aebcca93b1bedeae02eda7ca48220
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-02-20 18:06:04 +0100

    routes api

--- routes/api/recipes.routes.js
+++ routes/api/recipes.routes.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+router.delete("/", (req, res, next) => {
+    // Delete a recipe
+})
+
+
 module.exports = router;
--- routes/api/tags.routes.js
+++ routes/api/tags.routes.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+
+router.post("/", (req, res, next) => {
+    // Add new tag
+})
+
+router.patch("/:tagId", (req, res, next) => {
+    // Edit tag name and color
+})
+
+router.delete("/:tagId", (req, res, next) => {
+    // Delete tag
+})
+
+
 module.exports = router;

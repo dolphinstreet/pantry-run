ea1bdc17e8de08e38687490d576f8eb931573100
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-02-22 00:21:46 +0100

    fix another bug in the signup

--- routes/pages/lists.routes.js
+++ routes/pages/lists.routes.js
@@ -4,11 +4,33 @@ const List = require('../../models/List.model')
 const User = require('../../models/User.model');
 const { isLoggedIn } = require("../middlewares/auth");
 
-router.get("/", isLoggedIn, (req, res, next) => {
-    // display all lists
+//We are on based on /lists
 
-    res.render("lists/list-of-lists")
+router.get("/", isLoggedIn, async (req, res, next) => {
+
+    const anotherList = {
+        name: "Pasta al sugo",
+        favorite: true,
+        user: req.session.currentUser
+    }
+    const aList = {
+        name: "Lasagna",
+        user: req.session.currentUser
+    }
+    const anotherNewList = await List.create(anotherList)
+    const aNewList = await List.create(aList)
+
+    // display all lists
+    const user = req.session.currentUser._id
+    const userLists = await List.find(
+        { user: user }, //find
+    )
+
+    //res.send(userLists)
+    res.render("lists/list-of-lists", { userLists })
 });
+
+
 
 router.get("/:listId", isLoggedIn, (req, res, next) => {
     // display list details

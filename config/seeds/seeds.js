c6b5eee821dd61cbf61d677dfc03948ea3718dcb
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-21 18:08:37 +0100

    create seed runner in package.json

--- config/seeds/seeds.js
+++ config/seeds/seeds.js
@@ -1,13 +1,17 @@
-const openConnection = require("../db/");
+const openConnection = require("../../db/");
 const mongoose = require("mongoose");
 const seedUsers = require("./user.seeds");
+const seedCategories = require("./category.seeds");
+const seedUnits = require("./unit.seeds");
 
 async function seedDatabase() {
     try {
         const db = await openConnection();
         console.log(`Succesfully connected to ${db.connection.name} database.`);
 
-        seedUsers();
+        await seedCategories();
+        await seedUnits();
+        await seedUsers();
 
         await mongoose.disconnect();
         console.log(`Succesfully disconnected from ${db.connection.name}`);

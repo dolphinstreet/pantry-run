6386e1d4f5cd5f781a0241320679144356f461b4
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-02-22 10:37:23 +0100

    routes for the list of lists

--- routes/pages/lists.routes.js
+++ routes/pages/lists.routes.js
@@ -6,36 +6,47 @@ const { isLoggedIn } = require("../middlewares/auth");
 
 //We are on based on /lists
 
+//Only for testing purposes
+// async function someLists(req, res, next) {
+
+//     const anotherList = {
+//         name: "Weekly",
+//         favorite: true,
+//         user: req.session.currentUser.id
+//     }
+//     const aList = {
+//         name: "Sunday's market",
+//         user: req.session.currentUser.id
+//     }
+//     const anotherNewList = await List.create(anotherList)
+//     const aNewList = await List.create(aList)
+// }
+//res.send(aNewList)
 router.get("/", isLoggedIn, async (req, res, next) => {
-    const anotherList = {
-        name: "Weekly",
-        favorite: true,
-        user: req.session.currentUser.id
+    try {
+        // // display all lists
+        const user = req.session.currentUser.id
+        const userLists = await List.find(
+            { user } //find
+        )
+        res.render("lists/list-of-lists", { userLists })
+    } catch (error) {
+        next(error)
     }
-    const aList = {
-        name: "Sunday's market",
-        user: req.session.currentUser.id
-    }
-    const anotherNewList = await List.create(anotherList)
-    const aNewList = await List.create(aList)
-    //res.send(aNewList)
-
-    // // display all lists
-    const user = req.session.currentUser.id
-    const userLists = await List.find(
-        { user: user } //find
-    )
-
-    //res.send(userLists)
-    res.render("lists/list-of-lists", { userLists })
-    //res.send("ciao")
 });
 
 
 
-router.get("/:listId", isLoggedIn, (req, res, next) => {
+router.get("/:listId", isLoggedIn, async (req, res, next) => {
     // display list details
-    res.render("lists/list-details")
+    try {
+        const id = req.params.listId
+        const list = await List.findById(id)
+
+        res.render("lists/list-details", { list })
+    } catch (error) {
+        next(error)
+    }
 });
 
 router.get("/edit/:listId", isLoggedIn, (req, res, next) => {

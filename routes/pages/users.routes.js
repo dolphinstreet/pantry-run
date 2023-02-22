e0bc1d878fa184d41b565129002a002e4aa370c4
Chaudo <a.chaudoreille@gmail.com>
2023-02-22 17:34:34 +0100

    Saved list routes (#14)
    * saved lists routes

--- routes/pages/users.routes.js
+++ routes/pages/users.routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/User.model");
 
-router.get("/:userId", (req, res, next) => {
+router.get("/:userId", async (req, res, next) => {
     // profile consultation page
+    try {
+        const user = req.session.currentUser.id;
+        const userLists = await User.find({});
+        res.render("saved/list-of-lists", { userLists });
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get("/edit/:userId", (req, res, next) => {

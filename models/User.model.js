1e3f76096c7b7bc3bce795e33ad9a31872d549a0
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-22 17:58:22 +0100

    auto reformat on vsCode

--- models/User.model.js
+++ models/User.model.js
@@ -1,32 +1,34 @@
 const { Schema, model } = require("mongoose");
 
 // User model
-const userSchema = new Schema({
-    username: {
-        type: String,
-        trim: true,
-        required: false,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    photo: {
-        type: String,
-        default: "https://cdn-icons-png.flaticon.com/512/149/149071.png"
-    }
-},
+const userSchema = new Schema(
     {
-        // this second object adds extra properties: `createdAt` and `updatedAt`    
-        timestamps: true
-    });
+        username: {
+            type: String,
+            trim: true,
+            required: false,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        photo: {
+            type: String,
+            default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+        },
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true,
+    }
+);
 
 const User = model("User", userSchema);
 

e9dbf5338992935132157290650a29e754fae12d
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-22 19:33:08 +0100

    add event listener to check/uncheck items on list details

--- public/js/list-details.js
+++ public/js/list-details.js
@@ -1,14 +1,20 @@
-const listDetails = document.querySelector(".list-details")
-const listRow = document.querySelectorAll(".list-row")
-const checkedDiv = document.querySelector(".checked-items")
+const listDetails = document.querySelector(".list-details");
+const listRow = document.querySelectorAll(".list-row");
+const checkedDiv = document.querySelector(".checked-items");
+const uncheckedDiv = document.querySelector(".ingredients-by-category");
 
-
-listRow.forEach(row =>
-    row.addEventListener("touchstart", checkedItem)
-)
-
-function checkedItem(event) {
+function checkItem(event) {
     row = event.currentTarget;
-    checkedDiv.append(row.textContent)
-    // row.style.display = "none"
+    checkedDiv.append(row);
+    row.removeEventListener("touchstart", checkItem);
+    row.addEventListener("touchstart", uncheckItem);
 }
+
+function uncheckItem(event) {
+    row = event.currentTarget;
+    uncheckedDiv.append(row);
+    row.removeEventListener("touchstart", uncheckItem);
+    row.addEventListener("touchstart", checkItem);
+}
+
+listRow.forEach((row) => row.addEventListener("touchstart", checkItem));

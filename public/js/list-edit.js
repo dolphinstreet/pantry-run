72168deebdaa9f8821b1d64a29f62cbdbe892353
Chaudo <a.chaudoreille@gmail.com>
2023-02-23 15:55:30 +0100

    remove unnecessary try catch bloc and remove comments (#27)

--- public/js/list-edit.js
+++ public/js/list-edit.js
@@ -9,51 +9,44 @@ const rowTemplate = document.getElementById("template");
 const addbutton = document.querySelector(".fa-plus");
 const searchBar = document.querySelector("input[name=search]");
 
-saveButton.addEventListener("click", async function (event) {
-    const listRow = document.querySelectorAll(".list-row");
+saveButton.addEventListener("click", function (event) {
     event.preventDefault();
+
+    const listRow = document.querySelectorAll(".list-row");
     const redirect = event.currentTarget.href;
-    console.log(editableElements);
-    try {
-        const listInfo = {
-            "_id": listId, //id de la liste,
-            name: title.innerText,
-            //favorite: favorite.classList.contains("fa-solid"),
-            rows: [],
-        };
+    const listInfo = {
+        "_id": listId, //id de la liste,
+        name: title.innerText,
+        //favorite: favorite.classList.contains("fa-solid"),
+        rows: [],
+    };
 
-        listRow.forEach((item) => {
-            const amountDom = item.querySelector(".item-amount");
-            const unitDom = item.querySelector(".item-unit");
-            const ingredientDom = item.querySelector(".item-name");
+    listRow.forEach((item) => {
+        const amountDom = item.querySelector(".item-amount");
+        const unitDom = item.querySelector(".item-unit");
+        const ingredientDom = item.querySelector(".item-name");
 
-            listInfo.rows.push({
-                "_id": item.dataset.id,
-                amount: Number(amountDom.innerText),
-                unit: {
-                    "_id": unitDom.dataset.id,
-                    "name": unitDom.innerText,
-                },
-                ingredient: {
-                    "_id": ingredientDom.dataset.id,
-                    "name": ingredientDom.innerText,
-                },
-            });
+        listInfo.rows.push({
+            "_id": item.dataset.id,
+            amount: Number(amountDom.innerText),
+            unit: {
+                "_id": unitDom.dataset.id,
+                "name": unitDom.innerText,
+            },
+            ingredient: {
+                "_id": ingredientDom.dataset.id,
+                "name": ingredientDom.innerText,
+            },
         });
-        axios
-            .patch(`/api/lists/save`, listInfo)
-            .then((res) => {
-                console.log(res);
-                // check status for 200
-                window.location.href = redirect;
-            })
-            .catch((error) => {
-                //handle error
-                console.error(error);
-            });
-    } catch (error) {
-        console.log(error);
-    }
+    });
+    axios
+        .patch(`/api/lists/save`, listInfo)
+        .then((res) => {
+            window.location.href = redirect;
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
 
 addbutton.addEventListener("click", (event) => {

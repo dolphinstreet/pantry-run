8e61feb2166b46613ad0ddbffcfea61bf3c4bfeb
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-24 09:15:31 +0100

    merge resolution

--- public/js/script.js
+++ public/js/script.js
@@ -2,21 +2,37 @@ axios.defaults.withCredentials = true;
 
 /////////////////////////////////////////////////////////
 
-const settingsButton = document.querySelector(".details .fa-ellipsis-vertical")
-const settingsMenu = document.querySelector(".details .settings")
+const settingsButton = document.querySelector(".details .fa-ellipsis-vertical");
+const settingsMenu = document.querySelector(".details .settings");
 
-settingsButton.addEventListener("click", openMenu)
+settingsButton.addEventListener("click", openMenu);
+
+// settingsButton.addEventListener("click", openMenu);
+
+//settingsButton.addEventListener("click", closeMenu)
 
 function closeMenu(event) {
     settingsMenu.style.transform = "translateY(-100%)";
-    settingsButton.style.color = "#75a0ff"
-    event.currentTarget.removeEventListener("click", closeMenu)
-    event.currentTarget.addEventListener("click", openMenu)
+    settingsButton.style.color = "#75a0ff";
+    event.currentTarget.removeEventListener("click", closeMenu);
+    event.currentTarget.addEventListener("click", openMenu);
 }
 
 function openMenu(event) {
     settingsMenu.style.transform = "translateY(0)";
-    settingsButton.style.color = "white"
-    event.currentTarget.removeEventListener("click", openMenu)
-    event.currentTarget.addEventListener("click", closeMenu)
+    settingsButton.style.color = "white";
+    event.currentTarget.removeEventListener("click", openMenu);
+    event.currentTarget.addEventListener("click", closeMenu);
 }
+
+// document
+//     .querySelector(".fa-ellipsis-vertical")
+//     .addEventListener("click", (event) => {
+//         const id = document.querySelector(".list-details.details").dataset.id;
+//         axios
+//             .post("/api/lists/save/" + id)
+//             .then((res) => {
+//                 console.log(res);
+//             })
+//             .catch((e) => console.error(e));
+//     });

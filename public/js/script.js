975dc6a922e7ff53a9b6056b727a9a6421c7cfd0
Chaudo <a.chaudoreille@gmail.com>
2023-02-24 09:26:03 +0100

    Save template (#48)
    * template save route
    
    * save template liste

--- public/js/script.js
+++ public/js/script.js
@@ -2,21 +2,42 @@ axios.defaults.withCredentials = true;
 
 /////////////////////////////////////////////////////////
 
-const settingsButton = document.querySelector(".details .fa-ellipsis-vertical")
-const settingsMenu = document.querySelector(".details .settings")
+const settingsButton = document.querySelector(".details .fa-ellipsis-vertical");
+const settingsMenu = document.querySelector(".details .settings");
+const saveBtn = document.querySelector(".save-list-btn");
 
-settingsButton.addEventListener("click", openMenu)
+if (settingsButton) {
+    settingsButton.addEventListener("click", openMenu);
+}
+
+// settingsButton.addEventListener("click", openMenu);
+
+//settingsButton.addEventListener("click", closeMenu)
 
 function closeMenu(event) {
     settingsMenu.style.transform = "translateY(-100%)";
-    settingsButton.style.color = "#75a0ff"
-    event.currentTarget.removeEventListener("click", closeMenu)
-    event.currentTarget.addEventListener("click", openMenu)
+    settingsButton.style.color = "#75a0ff";
+    event.currentTarget.removeEventListener("click", closeMenu);
+    event.currentTarget.addEventListener("click", openMenu);
 }
 
 function openMenu(event) {
     settingsMenu.style.transform = "translateY(0)";
-    settingsButton.style.color = "white"
-    event.currentTarget.removeEventListener("click", openMenu)
-    event.currentTarget.addEventListener("click", closeMenu)
+    settingsButton.style.color = "white";
+    event.currentTarget.removeEventListener("click", openMenu);
+    event.currentTarget.addEventListener("click", closeMenu);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        const id = document.querySelector(".list-details.details").dataset.id;
+        axios
+            .post("/api/lists/save/" + id)
+            .then((res) => {
+                settingsButton.click();
+                console.log(res);
+            })
+            .catch((e) => console.error(e));
+    });
 }
